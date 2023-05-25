4c1ca254714d05d61912cc615c2c4034217f87ff
Maxwell Lou <maxlou2005@gmail.com>
2023-05-25 12:42:00 -0400

    clean up dead files

--- backend/tests/randtest.js
+++
@@ -1,9 +0,0 @@
-// function isBool(value) {
-//     if(value !== true && value !== false) throw new Error('must be true or false')
-// }
-
-// isBool(1)
-// isBool(123)
-
-let x = undefined
-if(x != null) console.log("yay")
